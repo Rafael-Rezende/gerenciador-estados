877a94eda7bd081a871ebf4c1ddce85c91d813af
Rafael Rezende <rafaeldeabreurezende@hotmail.com>
2021-08-30 21:30:27 -0300

    add counter functionality

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/internal/Observable';
+import { decrement, increment, reset } from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
-  templateUrl: './my-counter.component.html',
-  styleUrls: ['./my-counter.component.css']
+  templateUrl: './my-counter.component.html'
 })
 export class MyCounterComponent implements OnInit {
+  count$: Observable<number>
 
-  constructor() { }
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
 
   ngOnInit(): void {
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
   }
 
 }
